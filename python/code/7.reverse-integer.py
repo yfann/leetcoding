445e255ee1384e8ad6f9ccca0e359a92f49d7580
yfann <yfann2012@163.com>
2024-11-03 11:42:54 +0800

    update

---
+++ python/code/7.reverse-integer.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=7 lang=python
+#
+# [7] Reverse Integer
+#
+
+# @lc code=start
+class Solution(object):
+    def reverse(self, x):
+        li=[]
+        pos=1
+        if x==0:
+            return 0
+        if x<0:
+            pos=-1
+            x=-x
+        while x!=0:
+          mod=x%10
+          x=x//10
+          li.append(mod)
+        sum=0
+        l=len(li)
+        for i in li:
+          l-=1
+          sum+=i*(10**l)
+        return sum*pos
+
+        
+# @lc code=end
+
